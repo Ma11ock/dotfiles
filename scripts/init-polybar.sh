dbced0c962481b6bc5dd414f13e5780ec86dfcf9
Ryan Jeffrey <ryan@ryanmj.xyz>
2020-10-04 00:09:04 -0700

    1 polybar, toggle truncate lines, and menu bar

--- scripts/init-polybar.sh
+++ scripts/init-polybar.sh
@@ -7,13 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar main &
-  done
-else
-  polybar --reload main &
-fi
+polybar --reload main &
 
 #polybar main &
 
