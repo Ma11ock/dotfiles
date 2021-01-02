2e9dfd256db50cb5f22cd1239713de534d09d5a9
Ryan Jeffrey <ryan@ryanmj.xyz>
2021-01-02 13:51:04 -0800

    Emacs changes

--- scripts/init-polybar.sh
+++ scripts/init-polybar.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
+polybar_conf="/home/ryan/.config/polybar/desktop"
+
+case $HOSTNAME in
+    (Southpark) polybar_conf="/home/ryan/.config/polybar/desktop" ;;
+    (*) polybar_conf="/home/ryan/.config/polybar/config" ;;
+esac
+
 # Terminate already running bar instances
 killall -q polybar
 
@@ -7,8 +14,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar --reload main &
-
-#polybar main &
+polybar --reload main -c "$polybar_conf" &
 
 echo "Polybar launched..."
