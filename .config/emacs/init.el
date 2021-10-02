ea750827354041e0715248d638432ce95b17cbf3
Ryan Jeffrey <ryan@ryanmj.xyz>
2021-10-02 13:49:21 -0700

    Themes and evil mode fix, midi for mpd

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -26,9 +26,12 @@
     (package-refresh-contents)
     (package-install 'quelpa-use-package))
 
+(quelpa
+ '(quelpa-use-package
+   :fetcher git
+   :url "https://github.com/quelpa/quelpa-use-package.git"))
+(require 'quelpa-use-package)
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-
-
 
 (put 'narrow-to-region 'disabled nil)
 
@@ -46,7 +49,7 @@
  '(org-export-with-sub-superscripts '{})
  '(org-time-stamp-custom-formats '("<%b %e %G>" . "<%m/%d/%y %a %H:%M>"))
  '(package-selected-packages
-   '(evil-collection evil-magit lsp-mode gdscript-mode cmake-ide ivy-rtags rtags unicode-fonts py-autopep8 blacken elpy lua-mode luarocks modus-themes highlight-doxygen org-plus-contrib f company-c-headers cmake-font-lock auctex-cluttex auctex-latexmk auctex-lua company-auctex auctex inf-clojure cider clojure-mode clojure-mode-extra-font-locking clojure-quick-repls clojure-snippets json-mode crontab-mode rainbow-mode impatient-mode vterm rust-mode systemd yasnippet-snippets yasnippet gitignore-mode company flycheck wc-mode htmlize fic-mode emojify org-bullets use-package-hydra use-package-ensure-system-package use-package-el-get use-package-chords undo-tree quelpa-use-package mu4e-query-fragments mu4e-overview mu4e-maildirs-extension mu4e-jump-to-list mu4e-conversation mu4e-alert modus-vivendi-theme modus-operandi-theme ivy-ycmd ivy-avy highlight fish-mode elfeed-web elfeed-score elfeed-protocol elfeed-org elfeed-goodies csgo-conf-mode counsel))
+   '(evil-terminal-cursor-changer evil-collection evil-magit lsp-mode gdscript-mode cmake-ide ivy-rtags rtags unicode-fonts py-autopep8 blacken elpy lua-mode luarocks modus-themes highlight-doxygen org-plus-contrib f company-c-headers cmake-font-lock auctex-cluttex auctex-latexmk auctex-lua company-auctex auctex inf-clojure cider clojure-mode clojure-mode-extra-font-locking clojure-quick-repls clojure-snippets json-mode crontab-mode rainbow-mode impatient-mode vterm rust-mode systemd yasnippet-snippets yasnippet gitignore-mode company flycheck wc-mode htmlize fic-mode emojify org-bullets use-package-hydra use-package-ensure-system-package use-package-el-get use-package-chords undo-tree quelpa-use-package mu4e-query-fragments mu4e-overview mu4e-maildirs-extension mu4e-jump-to-list mu4e-conversation mu4e-alert modus-vivendi-theme modus-operandi-theme ivy-ycmd ivy-avy highlight fish-mode elfeed-web elfeed-score elfeed-protocol elfeed-org elfeed-goodies csgo-conf-mode counsel))
  '(safe-local-variable-values
    '((eval add-to-list 'company-clang-arguments '"-std=c++17")
      (eval add-to-list 'company-clang-arguments "-std=c++17")
